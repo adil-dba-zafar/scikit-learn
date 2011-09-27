0d7f00832091742f97977326156c21b29e07dc6c
Lars Buitinck <L.J.Buitinck@uva.nl>
2011-09-27 15:27:11 +0200

    PU learning: preliminary implementation of the 1-DNF method
    NumPy indexing magic galore!

---
+++ sklearn/semisupervised/__init__.py
@@ -0,0 +1,7 @@
+"""
+:mod:`sklearn.semisupervised` contains various schemes for semisupervised
+classification: training classifiers on a combination of labeled and unlabeled
+samples.
+"""
+
+from .pu import OneDNFTransformer
